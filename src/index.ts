041a579c91353b983e8a716be520d54dd17e0676
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2021-09-26 22:01:40 +0800

    Add api to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { getChapterId } from './helpers';
 import * as novelPage from './novel-page';
 import * as chapterList from './chapter-list';
+import { parseChapterPage } from './chapter-page';
+import { getGenreList } from './genres';
+import { getAllSlugs } from './slugs';
 
 const scraper = {
   ...novelPage,
-  getChapterId,
   ...chapterList,
+  getChapterId,
+  parseChapterPage,
+  getGenreList,
+  getAllSlugs,
 };
 
 export default scraper;
